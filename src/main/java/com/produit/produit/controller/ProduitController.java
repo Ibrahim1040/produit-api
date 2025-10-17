02497a118e9449da1c4a92601f01eebb399f82ca
Ibrahim1040 <tanger1040@hotmail.com>
2025-10-17 23:48:55 +0200

    Initialisation du repo

---
+++ src/main/java/com/produit/produit/controller/ProduitController.java
@@ -0,0 +1,32 @@
+package com.produit.produit.controller;
+
+import com.produit.produit.model.Produit;
+import com.produit.produit.service.ProduitService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/produits")
+@RequiredArgsConstructor
+public class ProduitController {
+
+    private final ProduitService produitService;
+
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "Hello";
+    }
+
+    @GetMapping("/all")
+    public List<Produit> getAllProduits(){
+        return produitService.getAllProduits();
+    }
+
+    @PostMapping
+    public Produit createProduit(@RequestBody Produit produit){
+        return produitService.createProduit(produit);
+    }
+
+}
